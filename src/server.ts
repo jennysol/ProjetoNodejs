a501e1c0bb37f8dc9c8668d9f73c6889f6fa8a3c
jennysol <jennyssoliver@gmail.com>
2020-08-03 19:31:33 -0300

    Adicionando Congigurações

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,7 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Olá'});
-});
+app.get('/', (request, response) => response.json({ message: 'Olá'}));
 
 app.listen(3333, () => {
     console.log('<3 Server started on port 3333!')
