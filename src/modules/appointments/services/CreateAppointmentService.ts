54b6c6059750774ae4975891b160261c7ebb89ba
jennysol <jennyssoliver@gmail.com>
2020-09-22 11:23:12 -0300

    Reescrevendo repositorios

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -25,12 +25,11 @@ class CreateAppointmentService {
       throw new AppError ('This appointment is already booked');
     }
 
-    const appointment = appointmentsRepository.create({
+    const appointment =  await appointmentsRepository.create({
       provider_id,
       date: appointmentDate,
     });
 
-    await appointmentsRepository.save(appointment);
 
     return appointment;
   }
