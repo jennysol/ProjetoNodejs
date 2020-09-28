80d2aa8d1853ecb599668db2ff27c13748097f78
jennysol <jennyssoliver@gmail.com>
2020-09-28 17:17:53 -0300

    Pensando nos testes

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,13 @@
-test('sum two numbers', () => {
-    expect(1 + 2).toBe(3);
+import CreateAppointmentService from './CreateAppointmentService';
+
+// CI - Executar testes 
+
+describe('CreateAppointment', () => {
+    it('should be able to create a new appointment', () => {
+        
+    });
+
+    // it('should not be  able to create two appointment on the same time', () =>  {
+    //     expect(1 + 2).toBe(3);
+    // });
 });
